b7c1e85a56bf67f2adbd58860c55161ffc35d1d3
unknown <akilapanunayan@gmail.com>
2024-03-26 01:35:45 +0100

    corrections

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -9,7 +9,7 @@ const SectionWrapper = (Component, idName) =>
 				variants={staggerContainer()}
 				initial="hidden"
 				whileInView="show"
-				viewport={{ once: true, amount: 0.25 }}
+				viewport={{ once: false, amount: 0.25 }}
 				className={`overflow-hidden relative z-0`}
 			>
 				<span className="hash-span" id={idName}>
